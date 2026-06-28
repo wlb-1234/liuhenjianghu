e73c694b6fea47272c5176fa6d4a108c90bae282
user4704912598 <1149404392207306-user4704912598@noreply.coze.cn>
2026-06-28 15:30:43 +0800

    fix: 简化 Provider 组件，只保留必要的 Provider

--- client/components/Provider.tsx
+++ client/components/Provider.tsx
@@ -1,22 +1,15 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import { type ReactNode } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { WebOnlyColorSchemeUpdater } from './ColorSchemeUpdater';
-import { WebOnlyPrettyScrollbar } from './PrettyScrollbar'
-import { HeroUINativeProvider } from '@/heroui';
 
 function Provider({ children }: { children: ReactNode }) {
-  return <WebOnlyColorSchemeUpdater>
-    <WebOnlyPrettyScrollbar>
-      <AuthProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <HeroUINativeProvider>
-            {children}
-          </HeroUINativeProvider>
-        </GestureHandlerRootView>
-      </AuthProvider>
-    </WebOnlyPrettyScrollbar>
-  </WebOnlyColorSchemeUpdater>
+  return (
+    <AuthProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        {children}
+      </GestureHandlerRootView>
+    </AuthProvider>
+  );
 }
 
 export {
